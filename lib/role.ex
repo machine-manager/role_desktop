b3535061c3be53080504bd281b29fac727aa70b8
Ivan Kozik <ivan@ludios.org>
2017-05-06 19:20:05 +0000

    Don't log Chrome failing to connect to 127.0.0.1:9229

--- lib/role.ex
+++ lib/role.ex
@@ -131,6 +131,13 @@ defmodule RoleDesktop do
 
 				# Chrome tries to talk UPnP on all interfaces (probably for Chromecast); we don't want to log it
 				daddr 239.255.255.250 proto udp dport 1900 REJECT;
+
+				outerface lo {
+					# Chrome Developer Tools, when opened, repeatedly tries to
+					# connect to this; we don't want to log it.  Implemented in
+					# chrome/browser/devtools/device/devtools_android_bridge.cc
+					daddr 127.0.0.1 proto tcp syn dport 9229 REJECT;
+				}
 				""",
 		}
 	end
